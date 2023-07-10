40472e2bd19d85b808096b92d322c235c4074bf6
Lizge BOZKURT <bozkurtlizge@gmail.com>
2023-07-10 13:54:22 +0300

    Add project files.

---
+++ SqlTest/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SqlTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sqlText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.selectwrite = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // sqlText
+            // 
+            this.sqlText.Location = new System.Drawing.Point(117, 129);
+            this.sqlText.Name = "sqlText";
+            this.sqlText.Size = new System.Drawing.Size(557, 22);
+            this.sqlText.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SQL TESTER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // selectwrite
+            // 
+            this.selectwrite.FormattingEnabled = true;
+            this.selectwrite.ItemHeight = 16;
+            this.selectwrite.Location = new System.Drawing.Point(117, 257);
+            this.selectwrite.Name = "selectwrite";
+            this.selectwrite.Size = new System.Drawing.Size(557, 148);
+            this.selectwrite.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SqlTest.Properties.Resources.image;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.selectwrite);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sqlText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sqlText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox selectwrite;
+    }
+}
+
---
+++ SqlTest/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SqlTest
+{
+    public partial class Form1 : Form
+    {
+        String connectionString;
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (sqlText.Text != null)
+            {
+
+                string connectionString = "Data Source=DESKTOP-6PAB4RO;Initial Catalog=Dbsql_relationship;User ID=Sa;Password=1";
+                /*cmd.Parameters.AddWithValue("@ad",baglan );
+                baglan.Open();
+                cmd.ExecuteNonQuery();
+                baglan.Close();*/
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    MessageBox.Show(" VeriTabanı bağlantısı gerçekleştirildi. Bekleyiniz...");
+
+                    string sorgu = sqlText.Text;
+                    using (SqlCommand command = new SqlCommand(sorgu, connection))
+                    {
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        if (reader.HasRows)
+                        {
+                            // StringBuilder stringBuilder = new StringBuilder();
+
+                            while (reader.Read())
+                            {
+                              
+                                StringBuilder stringBuilder = new StringBuilder();
+
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    string value = reader[i].ToString();
+                                    stringBuilder.Append(value + " | ");
+                                }
+
+                                selectwrite.Items.Add(stringBuilder.ToString().Trim());
+                            }
+                        }
+
+                        reader.Close();
+                    }
+
+                    connection.Close();
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen select sorgunuzu giriniz.");
+            }
+        }
+    }
+}
+    
+
